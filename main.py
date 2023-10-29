cee94b51033ee692f62430011dcf58b6035b3129
Raúl Osuna <rosuna@suse.com>
2023-10-29 19:08:41 +0100

    Handle KeyError exceptions caused by unexpected characters

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ alphabet_dict = {row.letter: row.code for (index,row) in
  pandas.read_csv("nato_phonetic_alphabet.csv").iterrows()}
 
 #Get a word from the user to spell
-word = input("Enter a word you want spelled according to the NATO phonetical alphabet:\n").upper()
-print([alphabet_dict[letter] for letter in word])
+def generate_phonetic():
+    word = input("Enter a word you want spelled according to the NATO phonetical alphabet:\n").upper()
+    try:
+        print([alphabet_dict[letter] for letter in word])
+    except KeyError:
+        print("Sorry, only letters in the alphabet, please")
+        generate_phonetic()
+    else:
+        correct_word = True
+
+generate_phonetic()
